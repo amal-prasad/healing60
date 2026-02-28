cd3578500697a370732aa8e661b7a0d1e6dcc21f
Amal Prasad <amalprasad1998@gmail.com>
2026-03-01 00:18:32 +0530

    Updates

---
+++ _next/static/chunks/app/services/[slug]/page-11bbe72ae59d5114.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[647,160,308,404,385,327,979,469,571],{2136:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2136)}),_N_E=n.O()}]);
